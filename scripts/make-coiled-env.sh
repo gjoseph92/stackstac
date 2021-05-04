92267601c1f5e8ed39909bb585ee49fe584588bb
Gabe Joseph <gjoseph92@gmail.com>
2021-05-03 21:22:22 -0600

    update make env script

--- scripts/make-coiled-env.sh
+++ scripts/make-coiled-env.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# cat <<EOF > requirements.txt
-# dask[complete]
-# IPython
-# jupyter-server-proxy
-# EOF
 poetry export --without-hashes -E binder > requirements.txt
-# echo "stackstac==$(poetry version -s)" >> requirements.txt
-echo "git+https://github.com/gjoseph92/stackstac.git@1b838faf4b346538d553f105381b245116952b61#egg=stackstac[viz]" >> requirements.txt
+echo "stackstac==$(poetry version -s)" >> requirements.txt
 poetry run coiled env create -n stackstac --pip requirements.txt
 
 rm requirements.txt
