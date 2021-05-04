70be68fbaf33dc1fd2e6fea3e1c4f08865f0d0c2
Gabe Joseph <gjoseph92@gmail.com>
2021-05-03 21:21:01 -0600

    disable binder until coiled has websockets

--- docs/conf.py
+++ docs/conf.py
@@ -53,11 +53,21 @@ default_role = "py:obj"
 nbsphinx_prolog = """
 {% set docname = env.doc2path(env.docname, base=False) %}
 .. note::
-    You can run this notebook interactively here: |Binder|, or view & download the original `on Github <https://github.com/gjoseph92/stackstac/blob/main/docs/{{ docname }}>`_.
+    You can view & download the original notebook
+    `on Github <https://github.com/gjoseph92/stackstac/blob/main/docs/{{ docname }}>`_.
+"""
 
-.. |Binder| image:: https://mybinder.org/badge_logo.svg
-   :target: https://mybinder.org/v2/gh/gjoseph92/stackstac/main?urlpath=lab/tree/docs/{{ docname }}
-"""  # noqa: E501
+# TODO enable binder once Coiled supports websocket clusters over 443.
+# (Binder blocks outbound traffic on all ports besides 22, 80, and 443, so we can't connect to Coiled on 8786.)
+# nbsphinx_prolog = """
+# {% set docname = env.doc2path(env.docname, base=False) %}
+# .. note::
+#     You can run this notebook interactively here: |Binder|, or view & download the original
+#     `on Github <https://github.com/gjoseph92/stackstac/blob/main/docs/{{ docname }}>`_.
+
+# .. |Binder| image:: https://mybinder.org/badge_logo.svg
+#    :target: https://mybinder.org/v2/gh/gjoseph92/stackstac/main?urlpath=lab/tree/docs/{{ docname }}
+# """
 
 
 # -- Options for HTML output -------------------------------------------------
