b161aa2b1fcb7af2d54747b10ddf58b6916c5e49
Gabe Joseph <gjoseph92@gmail.com>
2021-05-07 15:06:09 -0600

    dev flag for coiled script

--- scripts/coiled-envs.py
+++ scripts/coiled-envs.py
@@ -1,6 +1,8 @@
 """
 Make Coiled software environments for stackstac in multiple regions,
 and upload all notebook files into a Coiled notebook (aka Job).
+
+Pass --dev to build off the latest commit on `origin/main` instead.
 """
 
 import asyncio
@@ -9,6 +11,7 @@ import subprocess
 from pathlib import Path
 import sys
 from typing import List
+import warnings
 
 import aiotools
 import coiled
@@ -79,23 +82,35 @@ async def make_coiled_stuff(
         print(f"Created notebook {name}")
 
 
-if __name__ == "__main__":
-    proc = subprocess.run(
-        "poetry export --without-hashes -E binder -E viz",
-        shell=True,
-        check=True,
-        capture_output=True,
-        text=True,
-    )
-    deps = proc.stdout.splitlines()
-
-    # TODO single-source the version! this is annoying
-    version = subprocess.run(
-        "poetry version -s", shell=True, check=True, capture_output=True, text=True
+def run(cmd: str, **kwargs) -> str:
+    return subprocess.run(
+        cmd, shell=True, check=True, capture_output=True, text=True, **kwargs
     ).stdout.strip()
-    print(f"Version: {version}")
-    deps += [f"stackstac[binder,viz]=={version}"]
+
+
+if __name__ == "__main__":
+    # TODO use click or something to make an actual CLI.
+    dev = len(sys.argv) == 2 and sys.argv[-1] == "--dev"
+
+    deps = run("poetry export --without-hashes -E binder -E viz").splitlines()
+
+    if dev:
+        name = "stackstac-dev"
+        subprocess.run("git fetch", shell=True, check=True)
+        main = run("git rev-parse main")
+        origin_main = run("git rev-parse origin/main")
+        if main != origin_main:
+            warnings.warn("Your local main branch is not up to date with origin/main")
+        print(f"Commit (origin/main): {origin_main}")
+        stackstac_dep = f"git+https://github.com/gjoseph92/stackstac.git@{origin_main}#egg=stackstac[binder,viz]"
+    else:
+        name = "stackstac"
+        # TODO single-source the version! this is annoying
+        version = run("poetry version -s")
+        print(f"Version: {version}")
+        stackstac_dep = f"stackstac[binder,viz]=={version}"
+    deps += [stackstac_dep]
 
     asyncio.run(
-        make_coiled_stuff(deps, regions=["us-west-2", "eu-central-1"], name="stackstac")
+        make_coiled_stuff(deps, regions=["us-west-2", "eu-central-1"], name=name)
     )
