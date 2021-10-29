3c15c95e89069b5e35b1fc6a43ffc334e06beb07
Gabe Joseph <gjoseph92@gmail.com>
2021-10-28 21:01:15 -0600

    remove notebooks from coiled script

--- scripts/coiled-envs.py
+++ scripts/coiled-envs.py
@@ -5,81 +5,12 @@ and upload all notebook files into a Coiled notebook (aka Job).
 Pass --dev to build off the latest commit on `origin/main` instead.
 """
 
-import asyncio
 import io
 import subprocess
-from pathlib import Path
 import sys
-from typing import List
 import warnings
 
-import aiotools
 import coiled
-
-
-async def create_software_environment_quiet(
-    cloud: coiled.Cloud[coiled.core.Async], **kwargs
-) -> None:
-    log = io.StringIO()
-    try:
-        await cloud.create_software_environment(log_output=log, **kwargs)
-    except Exception:
-        print(
-            f"Error creating software environment with {kwargs}:\n{log.getvalue()}",
-            file=sys.stderr,
-        )
-        raise
-
-    print(f"Built environment for {kwargs.get('backend_options')}")
-
-
-async def make_coiled_stuff(
-    deps: List[str],
-    region: str,
-    name: str,
-) -> None:
-    examples = Path(__file__).parent.parent / "examples"
-    docs = Path(__file__).parent.parent / "docs"
-    notebook_paths = list(examples.glob("*.ipynb")) + list(docs.glob("*.ipynb"))
-    notebooks = list(map(str, notebook_paths))
-    print(f"Notebooks: {notebooks}")
-
-    async with coiled.Cloud(asynchronous=True) as cloud:
-        name_software = name + "-notebook"
-        async with aiotools.TaskGroup(name="envs") as tg:
-            # Build all the software environments in parallel in multiple regions
-            tg.create_task(
-                create_software_environment_quiet(
-                    cloud,
-                    name=name_software,
-                    container="coiled/notebook:latest",
-                    pip=deps,
-                    backend_options={"region": region},
-                )
-            )
-
-            tg.create_task(
-                create_software_environment_quiet(
-                    cloud,
-                    name=name,
-                    pip=deps,
-                    backend_options={"region": region},
-                )
-            )
-
-        # Create job configuration for notebook
-        await cloud.create_job_configuration(
-            name=name,
-            software=name_software,
-            cpu=2,
-            gpu=0,
-            memory="8 GiB",
-            command=["/bin/bash", "start.sh", "jupyter", "lab"],
-            files=notebooks,
-            ports=[8888],
-            description="Example notebooks from the stackstac documentation",
-        )
-        print(f"Created notebook {name}")
 
 
 def run(cmd: str, **kwargs) -> str:
@@ -111,4 +42,15 @@ if __name__ == "__main__":
         stackstac_dep = f"stackstac[binder,viz]=={version}"
     deps += [stackstac_dep]
 
-    asyncio.run(make_coiled_stuff(deps, region="us-west-2", name=name))
+    log = io.StringIO()
+    try:
+        coiled.create_software_environment(
+            name=name, pip=deps, backend_options={"region": "us-west-2"}, log_output=log
+        )
+    except Exception:
+        print(
+            f"Error creating software environment:\n{log.getvalue()}",
+            file=sys.stderr,
+        )
+        raise
+    print(f"Built software environment for {stackstac_dep}")
